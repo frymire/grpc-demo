90525ebbc2d25d670bced819ffd04a0e78f974ff
Frymire, Mark E. <mark.frymire@dac.us>
2016-09-15 14:25:03 -0400

    Obsessively refactor

--- src/main/java/HelloWorldClient.java
+++ src/main/java/HelloWorldClient.java
@@ -12,16 +12,14 @@ import java.util.logging.Logger;
  * A simple client that requests a greeting from the {@link HelloWorldServer}.
  */
 public class HelloWorldClient {
+  
   private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
 
   private final ManagedChannel channel;
   private final GreeterGrpc.GreeterBlockingStub blockingStub;
 
-  /** Construct client connecting to HelloWorld server at {@code host:port}. */
   public HelloWorldClient(String host, int port) {
-    channel = ManagedChannelBuilder.forAddress(host, port)
-        .usePlaintext(true)
-        .build();
+    channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();
     blockingStub = GreeterGrpc.newBlockingStub(channel);
   }
 
@@ -31,40 +29,32 @@ public class HelloWorldClient {
 
   /** Say hello to server. */
   public void greet(String name) {
-    logger.info("Will try to greet " + name + " ...");
-    HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-    HelloReply response;
+    
+    logger.info("Sending: " + name + " ...");
+    
+    HelloRequest request = HelloRequest.newBuilder().setName(name).build();    
+    HelloReply response;    
     try {
       response = blockingStub.sayHello(request);
     } catch (StatusRuntimeException e) {
       logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
       return;
     }
-    logger.info("Greeting: " + response.getMessage());
-    try {
-      response = blockingStub.sayHelloAgain(request);
-    } catch (StatusRuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-      return;
-    }
-    logger.info("Greeting: " + response.getMessage());
+    
+    logger.info("Received (1): " + response.getMessage());    
   }
 
-  /**
-   * Greet server. If provided, the first element of {@code args} is the name to use in the
-   * greeting.
-   */
   public static void main(String[] args) throws Exception {
+    
+    /* Access a service running on the local machine on port 50051 */
     HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+    
     try {
-      /* Access a service running on the local machine on port 50051 */
-      String user = "world";
-      if (args.length > 0) {
-        user = args[0]; /* Use the arg as the name to greet if provided */
-      }
-      client.greet(user);
+      client.greet("Mark");
     } finally {
       client.shutdown();
     }
+    
   }
+  
 }
--- src/main/java/HelloWorldServer.java
+++ src/main/java/HelloWorldServer.java
@@ -1,5 +1,4 @@
 
-
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
@@ -7,71 +6,52 @@ import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-/**
- * Server that manages startup/shutdown of a {@code Greeter} server.
- */
 public class HelloWorldServer {
+  
   private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
-
-  /* The port on which the server should run */
   private int port = 50051;
   private Server server;
 
   private void start() throws IOException {
-    server = ServerBuilder.forPort(port)
-        .addService(new GreeterImpl())
-        .build()
-        .start();
+    
+    server = ServerBuilder.forPort(port).addService(new GreeterImpl()).build().start();
     logger.info("Server started, listening on " + port);
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
+    
+    Thread thread = new Thread() {
+      @Override public void run() {
         // Use stderr here since the logger may have been reset by its JVM shutdown hook.
         System.err.println("*** shutting down gRPC server since JVM is shutting down");
         HelloWorldServer.this.stop();
         System.err.println("*** server shut down");
       }
-    });
+    };
+    
+    Runtime.getRuntime().addShutdownHook(thread);
   }
 
   private void stop() {
-    if (server != null) {
-      server.shutdown();
-    }
+    if (server != null) server.shutdown();
   }
 
-  /**
-   * Await termination on the main thread since the grpc library uses daemon threads.
-   */
+  /** Await termination on the main thread since the grpc library uses daemon threads. */
   private void blockUntilShutdown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
-    }
+    if (server != null) server.awaitTermination();
   }
 
-  /**
-   * Main launches the server from the command line.
-   */
+  private class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+    @Override public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+      HelloReply reply = HelloReply.newBuilder().setMessage("Suck it, " + req.getName()).build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+
+  }
+  
   public static void main(String[] args) throws IOException, InterruptedException {
     final HelloWorldServer server = new HelloWorldServer();
     server.start();
     server.blockUntilShutdown();
   }
 
-  private class GreeterImpl extends GreeterGrpc.GreeterImplBase {
-
-    @Override
-    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-      HelloReply reply = HelloReply.newBuilder().setMessage("You suck, " + req.getName()).build();
-      responseObserver.onNext(reply);
-      responseObserver.onCompleted();
-    }
-
-    @Override
-    public void sayHelloAgain(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-      HelloReply reply = HelloReply.newBuilder().setMessage("You suck hard, " + req.getName()).build();
-      responseObserver.onNext(reply);
-      responseObserver.onCompleted();
-    }
-  }
 }
